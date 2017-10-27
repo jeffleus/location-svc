a5366978e9fdb9372481ffdf378883e9c08b7d7b
Jeff Leininger <jeffleus@gmail.com>
2017-10-26 17:12:59 -0700

    update object model for CRUD operations

--- Location.js
+++ Location.js
@@ -8,35 +8,40 @@ var sequelize = new Sequelize('FuelStation_STAN', Config.username, Config.passwo
         max: 10,
         min: 1,
         idle: 100
-    }
+    },
+    dialect: 'mysql'
 });
 
-var Sport = sequelize.define('sport', {
-  SportCodeID: { 
-	  type: Sequelize.STRING, 
+var Location = sequelize.define('location', {
+  LocationID: { 
+	  type: Sequelize.INTEGER,
+      autoIncrement: true,
 	  primaryKey: true, 
-	  field: 'SportCodeID' 
+	  field: 'LocationID' 
   }, 
-  description: { type: Sequelize.STRING, field: 'sportDescription' }, 
-  inSeasonStart: { type: Sequelize.DATE, field: 'InSeasonStartDate' }, 
-  inSeasonEnd: { type: Sequelize.DATE, field: 'InSeasonEndDate' },
-  offSeasonStart: { type: Sequelize.DATE, field: 'OffSeasonStartDate' }, 
-  offSeasonEnd: { type: Sequelize.DATE, field: 'OffSeasonEndDate' },
+  title: { type: Sequelize.STRING, field: 'Title' }, 
+  description: { type: Sequelize.STRING, field: 'Description' },
+  isPre: { type: Sequelize.BOOLEAN, field: 'isPre' },
+  isPost: { type: Sequelize.BOOLEAN, field: 'isPost' },
+  isHydration: { type: Sequelize.BOOLEAN, field: 'isHydration' },
+  isSnack: { type: Sequelize.BOOLEAN, field: 'isSnack' },
+  isStaff: { type: Sequelize.BOOLEAN, field: 'isStaff' },
+  isFree: { type: Sequelize.BOOLEAN, field: 'isFree' }
 }, {
-	tableName: 'SportCodes'
+	tableName: 'Locations'
 });
 
-var moduleName = "SPORT:";
+var moduleName = "LOCATION:";
 
 module.exports.get = function(id,filter) {
     if (!id) return list(filter);
     console.log(moduleName, 'calling getSingle with id: ' + id);
     return sequelize.sync().then(function() {
-        return Sport.findById(id).then(function(sport) {
-            console.info(moduleName, 'sport record found');
+        return Location.findById(id).then(function(location) {
+            console.info(moduleName, 'location record found');
             return {
-                count: (sport)?1:0,
-                sports: [ (sport)?sport.dataValues:null ]
+                count: (location)?1:0,
+                locations: [ (location)?location.dataValues:null ]
             };
         })
     });
@@ -48,46 +53,44 @@ function list(filter) {
         if (filter) {
             var filterOption = {
                 where: {
-                    SportCodeID: filter 
+                    LocationID: filter 
                 } 
             };
-            return Sport.findAndCountAll(filterOption);
-        } else return Sport.findAndCountAll();
+            return Location.findAndCountAll(filterOption);
+        } else return Location.findAndCountAll();
     }).then(function(result) {
-		//return Athlete.findAndCountAll().then(function(result) {
-        var sports = [];
-        result.rows.forEach(function(sportRow) {
-            sports.push(sportRow.dataValues);
+        var locations = [];
+        result.rows.forEach(function(locationRow) {
+            locations.push(locationRow.dataValues);
         });
         return {
             count: result.count,
-            sports: sports
+            locations: locations
         };
 	});
 }
 
 module.exports.create = function(json) {
 	return sequelize.sync().then(function() {
-		console.info(moduleName, 'create a new sport using JSON provided');
-		console.error('need to add json validation to sport creation');
-		var sportJson = json;//JSON.parse(json);
-		return Sport.create(json).then(function(sport) {
-			console.info('sport successfully created');
-			return sport;
+		console.info(moduleName, 'create a new location using JSON provided');
+		console.error('need to add json validation to location creation');
+		var locationJson = json;//JSON.parse(json);
+		return Location.create(json).then(function(location) {
+			console.info('location successfully created');
+			return location;
 		});
 	});
 };
 
 module.exports.update = function(json) {
 	return sequelize.sync().then(function() {
-		console.info(moduleName, 'update a single sport using JSON provided');
-		console.error('need to add json validation to sport update');
-		var ath = json;//JSON.parse(json);
-		return Sport.update(
+		console.info(moduleName, 'update a single location using JSON provided');
+		console.error('need to add json validation to location update');
+		return Location.update(
 			json,
-			{ where: { SportCodeID: json.SportCodeID } }
+			{ where: { LocationID: json.LocationID } }
 		).then(function(result) {
-			console.info(moduleName, 'sport successfully updated');
+			console.info(moduleName, 'location successfully updated');
 			return result;
 		});
 	});
@@ -95,9 +98,9 @@ module.exports.update = function(json) {
 
 module.exports.delete = function(id) {
 	return sequelize.sync().then(function() {
-		console.info(moduleName, 'delete a sport by id');
-		return Sport.destroy({ where: { SportCodeID: id } }).then(function(count) {
-			console.info(moduleName, '(' + count.toString() + ') sports successfully deleted');
+		console.info(moduleName, 'delete a location by id');
+		return Location.destroy({ where: { LocationID: id } }).then(function(count) {
+			console.info(moduleName, '(' + count.toString() + ') location successfully deleted');
 			return count;
 		});
 	});
